0d61b4bcb45d7c483b47d2f27f266f8bc18381c1
brandylarramore <brandy.larramore@gmail.com>
2023-10-26 21:05:16 -0400

    working on 5

--- scripts/lahmans_baseball.sql
+++ scripts/lahmans_baseball.sql
@@ -92,26 +92,101 @@ FROM (
 
 
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
-   
+
+
+SELECT height, playerid, namefirst, namelast, teamid, g_batting, g_defense
+FROM people
+FULL JOIN appearances
+USING (playerid)
+WHERE height IS NOT NULL
+ORDER BY height
+LIMIT 1;
+
+
+
+--Eddie Gaedel. Played one game for the Saint Lewis Cardinals.
+
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+
+SELECT schoolname, schoolid
+FROM schools
+WHERE LOWER(schoolname) = 'vanderbilt university'
+
+--school id: vandy
+
+
+SELECT
+    c.schoolid,
+    c.playerid,
+    SUM(s.salary),
+    p.namefirst,
+    p.namelast
+FROM
+    schools
+INNER JOIN
+    collegeplaying AS c
+ON schools.schoolid = c.schoolid
+INNER JOIN
+    salaries AS s
+ON c.playerid = s.playerid
+INNER JOIN
+    people AS p
+ON s.playerid = p.playerid
+WHERE c.schoolid = 'vandy'
+GROUP BY c.schoolid, c.playerid, p.namefirst, p.namelast
+ORDER BY SUM(s.salary) DESC;
+	
 	
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
-   
+
+SELECT 
+    CASE 
+        WHEN pos = 'OF' THEN 'Outfield'
+        WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
+        WHEN pos IN ('P', 'C') THEN 'Battery'
+        ELSE 'Other'
+    END AS position,
+    SUM(po) AS total_putouts
+FROM fielding
+WHERE yearid = 2016
+GROUP BY position;
+
+
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
-   
+
+
+  
+1920 -2 
+2010 - 7
 
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
-	
+stolen bases is FROM batting
+Chris Ellings 91%?
+stolen bases + being caught stealing / both of     them added together stolen bases * 100 / sum of them together
+Persentage and then rank?
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+
+largest did not win -seattle maraners 116 2001
+smallest that did -dogers 1981
+
+2006 cardinals?
 
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
 
+
+
+
+
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+
+
+
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
